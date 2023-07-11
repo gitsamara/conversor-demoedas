1c11d3e2e3c8f68676b6aa2b101761886dbd2cc1
Samara Sales <101931584+gitsamara@users.noreply.github.com>
2023-07-11 11:12:28 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function Converter() {
+  var valorElemento = document.getElementById("valor");
+  var valor = valorElemento.value;
+  var valorEmReal = parseFloat(valor);
+  var valorEmDolar = valorEmReal / 5.02;
+
+  var valorConvertido = valorEmDolar.toFixed(2);
+
+  console.log(valorConvertido);
+
+  var elementoValorConvertido = document.getElementById("valorConvertido");
+  var valorConvertido = "O resultado em dólar é $" + valorConvertido;
+  elementoValorConvertido.innerHTML = valorConvertido;
+}
